ff721c88e3a0d1f270bf2a0fe3601b6221daaaee
Sonia RM Karungi <35847046+SoniaRMK@users.noreply.github.com>
2018-03-02 08:41:29 +0300

    Fixed bugs in test files

--- test_users.py
+++ test_users.py
@@ -15,14 +15,11 @@ class TestUserRegister(unittest.TestCase):
     def test_post(self):
 
         # valid: both required fields
-        user = [{"userEmail": "hjk@yyy.com", "userPassword": "jh123"}]
+        user = {"userEmail": "hjk@yyy.com", "userPassword": "jh123"}
         response = self.app.post(BASE_URL,
                                  data=json.dumps(user),
                                  content_type='application/json')
-        self.assertEqual(response.status_code, 400)
-        #data = json.loads(response.get_data())
-        # self.assertEqual(data["userEmail"], "hjk@yyy.com")
-        # self.assertEqual(data["userPassword"], "jh123")
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == "__main__":
