b2139f367127d183bfb04202ac6f5fa7e67ce5a9
Sonia RM Karungi <35847046+SoniaRMK@users.noreply.github.com>
2018-03-26 11:30:57 +0300

    User registration from non-persistent data to DB

--- resources/userAPI.py
+++ resources/userAPI.py
@@ -1,25 +1,126 @@
-from resources.lib import *
+from .resources import *
+import smtplib
+import random
+from flask_restful.reqparse import RequestParser
+from models.models import User
 
-users = [
-    {'userID' : 1, 'userEmail' : 'aws@xxx.com', 'userPassword' : 'qwer123' },
-    {'userID' : 2, 'userEmail' : 'hjk@yyy.com', 'userPassword' : 'jh123' }
-]
+
+token = ''
+
+#Validating the arguments
+user_validation = RequestParser(bundle_errors=True)
+#user_validation.add_argument("displayName", type=str, required=True, help="Name must be a valid string")
+user_validation.add_argument("user_email", type=str, required=True, help="Email must be a valid email")
+user_validation.add_argument("user_password", type=str, required=True, help="Password must be a valid string")
+
 
 class UserRegister(Resource):
-    def post(self):
-        user = {
-            'userID' : users[-1]['userID'] + 1,
-            'userEmail' : request.json['userEmail'],
-            'userPassword' : request.json['userPassword']
-            }
-        users.append(user)
-        return jsonify({'Users': users})
+    @swag_from("../APIdocs/CreateUser.yml")
+    def post(self): 
+       # user_args = user_validation.parse_args()
+        #user_email = user_validation.parse_args().user_email
+        #user_password = user_validation.parse_args().user_password
+        user = User(user_email=user_validation.parse_args().user_email, user_password=user_validation.parse_args().user_password)
 
+        """ if not user_email or not user_password:
+            message = {
+                'status': "Bad Request",
+                'message': 'User cannot be registered!',
+                }
+            resp = jsonify(message)
+            resp.status_code = 400
+            return resp """
+        try:
+            #user = models.User(user_email, user_password)
+            db.session.add(user)
+            db.session.commit()
+            message = {
+                'status': "Success",
+                'message': 'User registered!',
+                }
+            resp = jsonify(message)
+            resp.status_code = 201
+        except:
+            message = {
+                'status': "Server Error",
+                'message': 'User cannot be registered!',
+                }
+            resp = jsonify(message)
+            resp.status_code = 500 
+        
+        db.session.close()
+        return resp       
+
+        
 class UserLogin(Resource):
-    def post(self, email, psswd):
-        user = [u for u in users if u['userEmail'] == email and u['userPassword'] == psswd]
-        return jsonify({'user' : user[0]})
+    @swag_from("../APIdocs/LoginUser.yml")
+    def post(self):
+        useremail = user_validation.parse_args().user_email
+        userpassword = user_validation.parse_args().user_password
+
+        if not useremail or not userpassword:
+            message = {
+                'status': "Bad Request",
+                'message': 'Password and/or Email are missing!',
+                }
+            resp = jsonify(message)
+            resp.status_code = 401
+            return resp
+        
+        user = User.query.filter_by(email = useremail).first()
+        if not user:
+            message = {
+                'status': "Bad Request",
+                'message': 'User does not Exist!',
+                }
+            resp = jsonify(message)
+            resp.status_code = 401
+            return resp
+        if user.user_password == userpassword:
+            token = jwt.encode({
+                            'user' : user.id, 
+                            'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+        message = {
+            'status': "Success",
+            'message': 'Logged in',
+            'token' : token.decode('UTF-8'),
+            }
+        resp = jsonify(message)
+        resp.status_code = 200
+        return resp
 
 class UserLogout(Resource):
+    @swag_from("../APIdocs/LogoutUser.yml")
+    @token_required
     def post(self):
-        return jsonify({'Logged Out!!'})        
+        message = {
+            'status': "Success",
+            'message': 'Logged out',
+            }
+        resp = jsonify(message)
+        resp.status_code = 200
+        return resp
+
+class UserResetPassword(Resource):
+    @swag_from("../APIdocs/PasswordReset.yml")
+    def post(self):
+        resp = jsonify({})
+        user_args = user_validation.parse_args()
+        for user in users:
+            if user['userEmail'] == user_args.userEmail:
+                user['userPassword'] = user_args.userPassword
+                message = {
+                    'status': "Success",
+                    'message': 'Password Reset'
+                 }
+                resp = jsonify(message)
+                resp.status_code = 200
+            else:
+                message = {
+                    'status': "Not Found",
+                    'message': 'User doesnot exist',
+                    }
+                resp = jsonify(message)
+                resp.status_code = 404
+        return resp
