8d3969752d6f026b72fda383ebc1cd3d7e9994d6
Sonia RM Karungi <35847046+SoniaRMK@users.noreply.github.com>
2018-04-01 03:51:15 +0300

    Added manage.py to handle db migrations

---
+++ manage.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+from flask_script import Manager 
+from resources import app
+
+manage = Manager(app)
+
+@manage.command
+def migrator():
+    #os.system('set FLASK_APP=run'):
+        
+    #os.system('flask db init')
+    os.system('flask db migrate')
+    os.system('flask db upgrade')
+
+@manage.command
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('./tests' , pattern='test_user*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+if __name__ == '__main__':
+    manage.run()
