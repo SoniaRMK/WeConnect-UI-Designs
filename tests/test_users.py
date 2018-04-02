60d1f5852773d1e9e4b2950e48b0fff4e99d1a2a
Sonia RM Karungi <35847046+SoniaRMK@users.noreply.github.com>
2018-04-02 15:13:21 +0300

    User register tests with DB

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,26 +1,52 @@
 import unittest
 import json
-
-import app
-
-BASE_URL = '/api/v2/auth/register'
+from resources import app, db
+from run import UserRegister, UserLogin
+from models.models import User
 
 
 class TestUserRegister(unittest.TestCase):
 
     def setUp(self):
-        self.app = app.app.test_client()
-        self.app.testing = True
+        self.app=app.test_client()
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        db.create_all()
+        db.session.add(User(user_email='abc@yyyy.zzz', user_password='67890'))
+        db.session.add(User(user_email='def@yyy.zzz', user_password='12345'))
+        db.session.commit()
+        return app
 
-    def test_post(self):
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
-        # valid: both required fields
-        user = {"userEmail": "hjk@yyy.com", "userPassword": "jh123"}
-        response = self.app.post(BASE_URL,
-                                 data=json.dumps(user),
-                                 content_type='application/json')
-        self.assertEqual(response.status_code, 200)
+    def test_register_user_success(self):
+        """Ensures that a user is registered successfully"""
+        response = self.app.post('/api/v2/auth/register',
+                            content_type='application/json',
+                            data=json.dumps({"user_email": "soniakxxx@yyy.com", "user_password": "jh123"})
+                            )
+        self.assertEqual(response.status_code, 201)
+        self.assertIn(b'User registered!', response.data)
 
+    def test_register_user_exists(self):
+        """Ensures that a user is not registered twice"""
+        response = self.app.post('/api/v2/auth/register',
+                            content_type='application/json',
+                            data=json.dumps({"user_email": "abc@yyyy.zzz", "user_password": "67890"})
+                            )
+        self.assertEqual(response.status_code, 409)
+        self.assertIn(b'User already exists!', response.data)
 
+    def test_register_user_fail(self):
+        """Ensures that a user is not registered with missing credential"""
+        response = self.app.post('/api/v2/auth/register',
+                            content_type='application/json',
+                            data=json.dumps({"user_email": "afgc@yyyy.zzz"})
+                            )
+        self.assertEqual(response.status_code, 400)
+        self.assertIn(b'Password must be a valid string', response.data)
+       
 if __name__ == "__main__":
     unittest.main()
