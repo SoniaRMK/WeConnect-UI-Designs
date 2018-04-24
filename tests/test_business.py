63853e0a0c2f0ce96abd0fb17b28426145d098a2
Sonia RM Karungi <soniakxxx@gmail.com>
2018-04-24 06:05:49 +0300

    refactor tests

--- tests/test_business.py
+++ tests/test_business.py
@@ -24,10 +24,10 @@ class TestBusiness(unittest.TestCase):
     def test_create_business_no_auth(self):
         """Tests to ensure that a user not logged in can't create a business"""
         response = self.app.post('/api/v2/businesses',
-                            content_type='application/json',
-                            data=json.dumps({"business_name" : "InfoTech Electronics Ltd.", 
-                                "business_profile" : "Deals in Mobile phone and Laptop accessories", 
-                                "location" : "Kampala", "category" : "Telecommunications", "user_id" : 1})
+                        content_type='application/json',
+                        data=json.dumps({"business_name" : "InfoTech Electronics Ltd.", 
+                                        "business_profile" : "Deals in Mobile phone and Laptop accessories", 
+                                        "location" : "Kampala", "category" : "Telecommunications", "user_id" : 1})
                             )
         self.assertEqual(response.status_code, 404)
         self.assertIn(b'Token is missing!!', response.data)
