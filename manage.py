3ac7d4160ea90efc635e05f91b499031e86318e9
Sonia RM Karungi <shewolf227@Sonias-MacBook-Pro.local>
2018-05-04 09:39:35 +0300

    Updated .travis.yml file

--- manage.py
+++ manage.py
@@ -16,7 +16,7 @@ def migrator():
 @manage.command
 def test():
     """Runs the unit tests without test coverage."""
-    tests = unittest.TestLoader().discover('./tests' , pattern='test_user*.py')
+    tests = unittest.TestLoader().discover('./tests' , pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         return 0
